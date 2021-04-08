bb640938fabc48ed84cee37a8ecde610166b4b1e
Chun-Cheng <61136202+Chun-Cheng@users.noreply.github.com>
2021-04-08 15:50:30 +0800

    Update webpage.py

--- webpage.py
+++ webpage.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, redirect, url_for, render_template
+from flask import Blueprint, request, redirect, url_for, render_template, flash
 import function
 
 webpage = Blueprint('webpage', __name__)  # static_folder=None, static_url_path=None, template_folder=None, url_prefix=None, subdomain=None
@@ -6,3 +6,26 @@ webpage = Blueprint('webpage', __name__)  # static_folder=None, static_url_path=
 @webpage.route('/')
 def index():
     return 'This is myschool-account-webpage'
+
+
+@app.route('/signup', methods=['GET', 'POST'])
+def page_signup():
+    if request.method == 'POST':
+        first_name = request.form.get('first_name')
+        last_name = request.form.get('last_name')
+        email = request.form.get('email')
+        phone = request.form.get('phone')
+        password = request.form.get('password')
+        password_check = request.form.get('password_check')
+        
+        if password != password_check:
+            flash('確認密碼輸入錯誤')
+            return render_template('signup_html')
+        
+        _id = function.data_signup(first_name=first_name, last_name=last_name, email=email,  phone=phone, password=password)
+        
+        return '成功註冊!'
+        
+    else:
+        return render_template('signup.html')
+    
