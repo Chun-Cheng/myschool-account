a45f5025563b5f91b5c57d8ebc3286e97204e3a7
Chun-Cheng <61136202+Chun-Cheng@users.noreply.github.com>
2021-04-11 14:03:41 +0800

    Update function.py

--- function.py
+++ function.py
@@ -1,4 +1,5 @@
 import pymongo
+from werkzeug.security import generate_password_hash, check_password_hash
 
 client = pymongo.MongoClient('mongodb+srv://dbUser:o5jzqcHzuKacB2Y1@lunchbox.1pvyu.mongodb.net/lunchbox?retryWrites=true&w=majority')
 database = client.myaccount
@@ -17,15 +18,39 @@ def data_signup( first_name, last_name, email, phone, password ):
       data_id
     """
     global accounts
-
+    
+    find_result = accounts.find({'email':email})
+    if len(find_result) > 0:
+        return 'email have been exist!'
+    
     insert_data = { 'first_name' : first_name ,
                   'last_name' : last_name ,
                   'email' : email, 
                   'phone' : phone, 
-                  'password' : password }
+                  'password_hash' : generate_password_hash(password,'shake_256') }
 
     x = accounts.insert_one(insert_data)
     _id = x.inserted_id
 
     return _id
+
+def data_login_check(email, password):
+    """
+    確認用戶登入資料
+    input:
+      email
+      password
+    output:
+      email ERROR!
+      password ERROR!
+      find_result
+    """
+    global accounts
+    
+    find_result = accounts.find({'email':email}, {'_id':1, 'first_name':1, 'last_name':1, 'password_hash':1})
+    if len(find_result) == 0:
+        return 'email ERROR!'
+    if check_password_hash(find_result[0]['password_hash'], password) == False:
+        return 'password ERROR!'
+    return find_result[0]
 
