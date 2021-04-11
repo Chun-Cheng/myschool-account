e13c963f21c9571994298a97cb2230bd207aa710
Chun-Cheng <61136202+Chun-Cheng@users.noreply.github.com>
2021-04-11 14:06:26 +0800

    Update function.py

--- function.py
+++ function.py
@@ -19,7 +19,7 @@ def data_signup( first_name, last_name, email, phone, password ):
     """
     global accounts
     
-    find_result = accounts.find({'email':email})
+    find_result = list(accounts.find({'email':email}))
     if len(find_result) > 0:
         return 'email have been exist!'
     
@@ -47,7 +47,7 @@ def data_login_check(email, password):
     """
     global accounts
     
-    find_result = accounts.find({'email':email}, {'_id':1, 'first_name':1, 'last_name':1, 'password_hash':1})
+    find_result = list(accounts.find({'email':email}, {'_id':1, 'first_name':1, 'last_name':1, 'password_hash':1}))
     if len(find_result) == 0:
         return 'email ERROR!'
     if check_password_hash(find_result[0]['password_hash'], password) == False:
