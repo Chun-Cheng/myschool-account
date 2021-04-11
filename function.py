f77802f09e95084528a3fae7b437fc6989e47314
Chun-Cheng <61136202+Chun-Cheng@users.noreply.github.com>
2021-04-11 14:41:08 +0800

    Update function.py

--- function.py
+++ function.py
@@ -1,9 +1,12 @@
 import pymongo
+from bson.objectid import ObjectId
+from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 
 client = pymongo.MongoClient('mongodb+srv://dbUser:o5jzqcHzuKacB2Y1@lunchbox.1pvyu.mongodb.net/lunchbox?retryWrites=true&w=majority')
 database = client.myaccount
 accounts = database.accounts
+have_login = database.have_login
 
 def data_signup( first_name, last_name, email, phone, password ):
     """
@@ -27,7 +30,8 @@ def data_signup( first_name, last_name, email, phone, password ):
                   'last_name' : last_name ,
                   'email' : email, 
                   'phone' : phone, 
-                  'password_hash' : generate_password_hash(password,'sha3_512') }
+                  'password_hash' : generate_password_hash(password,'sha3_512'),
+                  'logins' : [] }
 
     x = accounts.insert_one(insert_data)
     _id = x.inserted_id
@@ -54,3 +58,24 @@ def data_login_check(email, password):
         return 'password ERROR!'
     return str(find_result[0]['_id'])
 
+def data_login_write(_id):
+    """
+    新增登入資料
+    input:
+      _id(accounts)
+    output:
+      token
+    """
+    global have_login
+    global accounts
+    
+    insert_data = { 'device' : 'Unknow' ,
+                    'login_time' : datetime.utcnow() ,
+                    'expire_time' : None, 
+                    'authorization' : 'ALL' }
+    x = accounts.insert_one(insert_data)
+    token = x.inserted_id
+    
+    accounts.update({'_id': ObjectId(_id)}, {'$push': {'logins': token}})
+    
+    return token
