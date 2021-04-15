fe15fb6a54686d1a44505ada24b4e381e35938e0
Chun-Cheng <61136202+Chun-Cheng@users.noreply.github.com>
2021-04-15 16:12:30 +0800

    Update api.py

--- api.py
+++ api.py
@@ -6,3 +6,24 @@ api = Blueprint('api', __name__, url_prefix='/api')  # static_folder=None, stati
 @api.route('/')
 def api_index():
     return 'This is myschool-account-api'
+
+
+@api.route('/token_check_request', methods=['POST'])
+def api_token_check_request():
+    token = str(request.args.get('token'))
+    return jsonify( {'result':function.data_login_find(token)} )
+
+
+@api.route('/account_data_request', methods=['POST'])
+def api_account_data_request():
+    token = str(request.args.get('token'))
+    cols = request.args.get('cols')
+    
+    query_return = {}
+    if len(cols) == 0:
+        query_return['first_name'] = 1
+    else:
+        for c in cols.keys():
+            query_return[c] = 1
+    
+    return jsonify(function.data_account_data_get(token, query_return))
